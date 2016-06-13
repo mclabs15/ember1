0a211fcb33c2d50d593331d6bf1196fea01fdca0
mclabs15 <mclabs15@gmail.com>
2016-06-13 13:53:07 -0700

    working on step 13

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -13,7 +13,8 @@ var PhotoCollection = Ember.ArrayProxy.extend(Ember.SortableMixin, {
 });
 
 var testPhotos = PhotoCollection.create();
-var testimg1 = Photo.create({
+
+/*var testimg1 = Photo.create({
 	title:"Google logo",
 	username:"google",
 	url:"https://www.google.com/images/srpr/logo11w.png"
@@ -29,14 +30,54 @@ var testimg3 = Photo.create({
 	title:"Orange Man",
 	username:"Orange",
 	url:"http://i.imgur.com/Ip4CbC5.jpg"
-});
-
+});*/
+/*
 testPhotos.pushObject(testimg1);
 testPhotos.pushObject(testimg2);
 testPhotos.pushObject(testimg3);
+*/
+var ps = [
+{
+	title:"Google logo",
+	username:"google",
+	url:"https://www.google.com/images/srpr/logo11w.png"
+},
+{
+	title:"UNO logo",
+	username:"UNO",
+	url:"http://unomaha.edu/_files/images/logo-subsite-o-2.png"
+},
+{
+	title:"Orange Man",
+	username:"Orange",
+	url:"http://i.imgur.com/Ip4CbC5.jpg"
+},
+{
+	title: "Hubble Carina Nebula",
+	username: "NASA",
+	url: "http://imgsrc.hubblesite.org/hu/db/images/hs-2010-13-a-1920x1200_wallpaper.jpg"
+}
 
+];
 
+for(var i = 0; i < ps.length; i++) {
+	testPhotos.pushObject(ps[i]);
+}
 
 export default Ember.Controller.extend({
-	photos: testPhotos
+	photos: testPhotos,
+	searchField: '',
+	filteredPhotos: testPhotos,
+	actions: {
+		search: function () {
+			var filter = this.get('searchField');
+			var rx = new RegExp(filter, 'gi');
+			var photos = this.get('photos');
+			this.set('filteredPhotos',
+				photos.filter(function(photo){
+					return photo.get('title').match(rx) || photo.get('username').match(rx);
+				})
+			);
+		}
+	}
 });
