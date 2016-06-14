318cc8f66b524c6d1dd2125212f46c4a5fd9df82
mclabs15 <mclabs15@gmail.com>
2016-06-14 13:49:42 -0700

    step 18 doen

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -16,8 +16,8 @@ import Ember from 'ember';
 });*/
 
 var PhotoCollection = Ember.ArrayProxy.extend(Ember.SortableMixin, {
-	sortProperties: ['title'],
-	sortAscending: true,
+	sortProperties: ['dates.taken'],
+	sortAscending: false,
 	content: [],
 });
 
@@ -25,13 +25,14 @@ export default Ember.Controller.extend({
 	photos: PhotoCollection.create(),
 	searchField: '',
 	tagSearchField:'',
+	tagList:[],
 	filteredPhotos: function () {
 		var filter = this.get('searchField');
 		var rx = new RegExp(filter, 'gi');
 		var photos = this.get('photos');
 
 		return photos.filter(function(photo){
-			return photo.get('title').match(rx) || photo.get('username').match(rx);
+			return photo.get('title').match(rx) || photo.get('owner.username').match(rx);
 		});
 	}.property('photos.@each','searchField'),
 	actions: {
@@ -43,19 +44,29 @@ export default Ember.Controller.extend({
 		getPhotos: function(tag){
 		var apiKey = 'c89d5f163cec11aa357c7ca65a07c052';
 		var host = 'https://api.flickr.com/services/rest/';
-		var method = "flickr.tags.getClusterPhotos";
-		var requestURL = host + "?method="+method + "&api_key="+apiKey+"&tag="+tag+"&format=json&nojsoncallback=1";
+		var method = "flickr.photos.search";
+		var requestURL = host + "?method="+method + "&api_key="+apiKey+"&tags="+tag+"&format=json&nojsoncallback=1";
 		var photos = this.get('photos');
 		var t = this;
+		console.log(requestURL);
 		Ember.$.getJSON(requestURL, function(data){
-			//callback for successfully completed requests
-			console.log(data);
-			data.photos.photo.map(function(photo) {
+		//callback for successfully completed requests
+		//make secondary requests to get all of the photo information
+		data.photos.photo.map(function(photoitem) {//iterate over each photo
+			var infoRequestURL = host + "?method="+"flickr.photos.getInfo" + "&api_key="+apiKey+ "&photo_id="+photoitem.id+"&format=json&nojsoncallback=1";
+			Ember.$.getJSON(infoRequestURL, function(item){
+				var photo = item.photo;
+				var tags = photo.tags.tag.map(function(tagitem){
+					return tagitem._content;
+				});
 				var newPhotoItem = t.store.createRecord('photo',{
-					title: photo.title,
-					username: photo.username,
-					//flickr extra data
+					title: photo.title._content,
+					dates: photo.dates,
 					owner: photo.owner,
+					description: photo.description._content,
+					link: photo.urls.url[0]._content,
+					views: photo.views,
+					tags: tags,
 					//flickr url data
 					id: photo.id,
 					farm: photo.farm,
@@ -65,9 +76,32 @@ export default Ember.Controller.extend({
 				photos.pushObject(newPhotoItem);
 			});
 		});
+	});
+
 	},
+	clicktag: function(tag){
 
-
-
+	this.set('tagSearchField', tag);
+	this.get('photos').content.clear();
+	this.store.unloadAll('photo');
+	this.send('getPhotos',tag);
 	}
+
+
+	},
+	init: function(){
+	this._super.apply(this, arguments);
+	var apiKey = '4435e3a217bc7afc94dfcba607b70eb1';
+	var host = 'https://api.flickr.com/services/rest/';
+	var method = "flickr.tags.getHotList";
+	var requestURL = host + "?method="+method + "&api_key="+apiKey+"&count=75&format=json&nojsoncallback=1";
+	var t = this;
+	Ember.$.getJSON(requestURL, function(data){
+		//callback for successfully completed requests
+		console.log(data);
+		data.hottags.tag.map(function(tag) {
+			t.get('tagList').pushObject(tag._content);
+		});
+	});
+}
 });
--- config/environment.js
+++ config/environment.js
@@ -20,10 +20,10 @@ module.exports = function(environment) {
     
   contentSecurityPolicy: {
     'default-src': "'self' ",
-    'script-src': "'self' https://api.flickr.com/*", // Allow scripts from self only, this prevents injection and XSS attacks
+    'script-src': "'self' ", // Allow scripts from self only, this prevents injection and XSS attacks
     'font-src': "'self' http: https:", // Allow fonts to be loaded from self or any http/https source
-    'connect-src': "'self' ", // Allow data (ajax/websocket) from self only (The same IP as this app)
-    'img-src': "'self' http: https:", //Allow images from any http/https server
+    'connect-src': "'self' https://api.flickr.com", // Allow data (ajax/websocket) from self only (The same IP as this app)
+    'img-src': "'self' http: https:  https://api.flickr.com", //Allow images from any http/https server
     'style-src': "'self' ", // Allow inline styles and loaded CSS from same IP
     'media-src': "'self' "
   }
